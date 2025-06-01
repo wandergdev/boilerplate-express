1bc3672b03691ffa8962c36b77b8a23b7492ce2e
wandergdev <wanderg.dev@gmail.com>
2025-06-01 14:13:01 -0400

    Enviando un archivo por el metodo get que coincida con la ruta raiz

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
 
+absolutePath = __dirname + '/views/index.html'
 app.get("/", function(req, res){
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
 });
 
 module.exports = app;
