4c63ef562ff0c5a59db78363a948151ff1556ee9
wandergdev <wanderg.dev@gmail.com>
2025-06-01 15:32:09 -0400

    Api Simple para devolver un json

--- myApp.js
+++ myApp.js
@@ -12,6 +12,10 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/json', function(req, res){
+    res.json({mesagge: "Hello Json"})
+});
+
 
 
 
