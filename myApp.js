0921b0e4f508d01ffef3240a6374d530e9cd49c9
wandergdev <wanderg.dev@gmail.com>
2025-06-01 16:18:05 -0400

    Implementacion de un Middleware de registro de peticiones a nivel raíz

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ let app = express();
 //     res.send("Hello Express")
 // });
 
+app.use(function(req, res, next){
+    console.log(`${req.method} ${req.path} - ${req.ip}` );
+    next();
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
